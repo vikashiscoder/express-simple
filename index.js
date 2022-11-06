dab169b9890a21b62cf86b6df1915b46e0905342
vikashiscoder <39021552+vikashiscoder@users.noreply.github.com>
2022-11-06 21:59:40 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3010;
+const port = process.env.PORT || 3030;
 const path = require('path');
 
 app.use(express.static('static'));
